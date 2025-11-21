4e798d56c8db711798c225c3c825f16bd30323b6
drxg0nG <kaavishdeepan@gmail.com>
2025-11-21 08:59:38 -0500

    cw

---
+++ Lesson5-Exception_Handling/common-exceptions.py
@@ -0,0 +1,36 @@
+# # Common Errors
+# # 1 - Division by zero
+# print(10/0)
+
+# # 2 - Value Error
+# print(int("abc"))
+
+# # 3 - Key Error
+# grades = {"Bob": 90}
+# print(grades["Bill"])
+
+# # 4 - Index Error
+# numbers = [1, 2, 3]
+# print(numbers[5])
+
+# # 5 - Name Error
+# print(total)
+# total = 10
+
+# # 6 - TypeError
+# print("hello" + 5)
+
+# # 7 - Syntax Error (cannot run directly, will crash)
+# for i in range(5)
+# print(i)
+
+# # 8 - Attribute Error
+# x = 5
+# x.append(3)
+
+# # 9 ModuleNotFound
+# import wonka_factory
+
+# # 10 - IndentationError
+# if i > 0:
+# return i
--- Lesson5-Exception_Handling/exceptions.py
+++ Lesson5-Exception_Handling/exceptions.py
@@ -1,23 +1,63 @@
-# # Common Errors
-# # 1 - Division by zero
-# print(10/0)
+def safe_divide(a, b):
+    try:
+        result = a / b
+        return result
+    # except:
+    #     print("Cannot divide by zero!")
+    #     return None
+    except ZeroDivisionError:
+        print("Cannot divide by zero!")
+        return None
+    except TypeError:
+        print("That's not a valid number")
+        return None
+    except:
+        print("An error occured...")
 
-# # 2 - Value Error
-# print(int("abc"))
+print(safe_divide(10, 2)) # 5.0
+print(safe_divide(10, 0)) # Cannot divide by zero!
 
-# # 3 - Key Error
-# grades = {"Bob": 90}
-# print(grades["Bill"])
 
-# # 4 - Index Error
-# numbers = [1, 2, 3]
-# print(numbers[5])
+def safe_operations(a, b, lst, key, d):
+    try:
+        print(f"Divisionn Result: {a / b}") # ZeroDivisionError, TypeError
+        print("Access list element:", lst[2]) # IndexError
+        print("Access dictionary key:", d[key]) # KeyError
+        print("Add numbers: {a + b}") # 
+    except ZeroDivisionError:
+        print("Cannot divide by zero")
+    except IndexError:
+        print("Lisy index out of range")
+    except KeyError:
+        print(f"Key {key} not found in dictionary")
+    except TypeError:
+        print("Invalid types for operation")
+    except Exception as e:
+        print("Some other error occured", e)
 
-# # 5 - Name Error
-# print(total)
-# total = 10
+print(10, 2, [1, 2], "Tom", {"John":15})
 
-# # 6 - Syntax Error
-# for i in range(5)
-# print(i)
+
+def calculate_price_per_item(cost, quantity):
+    try:
+        unit_price = cost / quantity
+        return unit_price
+    except ZeroDivisionError:
+        return "No items to calculate"
+print(calculate_price_per_item(100, 4))
+print(calculate_price_per_item(50, 0))
+print(calculate_price_per_item(25.50, 3))
+
+
+def parse_age(age):
+    try:
+        num_age = int(age)
+        return num_age
+    except ValueError:
+        return None
+
+print(parse_age("25"))
+print(parse_age("tweety-five"))
+
+
 
